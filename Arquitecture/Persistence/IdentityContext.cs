0e07065ac5f859717f174f5d982fa7215f3e8b6d
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2022-07-12 13:34:48 -0400

    adding identity

---
+++ Arquitecture/Persistence/IdentityContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace Persistence;
+
+public class IdentityContext : IdentityDbContext
+{
+    public IdentityContext()
+    {}
+
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    {}
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            optionsBuilder.UseSqlServer("Server=192.168.137.1;Database=VHM;User Id=sa;Password=Masterxp01;Trusted_Connection=True;Integrated Security=False");
+        }
+    }
+}
--- VHM.Api/Program.cs
+++ VHM.Api/Program.cs
@@ -1,3 +1,13 @@
+using Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<VHMContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("VHMContext")));
+builder.Services.AddDbContext<IdentityContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("VHMContext")));
 
 var app = builder.Build();
 
