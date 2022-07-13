df8cb7e7d23af43a3de4d5ee31a3f5c0c82a2314
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2022-07-13 11:41:24 -0400

    contruyendo vistas de autenticacion

--- VHM.Web/src/app/app-routing.module.ts
+++ VHM.Web/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+	path:"/",
+	redirectTo: "auth",
+	pathMatch: "full",
+	children:[
+	    {
+		path:"home",
+	    }
+	]
+    },
+    {
+	path:"auth",
+	loadChildren: ()=> import("./auth/auth.module").then(x => x.AuthModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
