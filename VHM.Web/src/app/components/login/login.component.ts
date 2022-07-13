07a08834c35f2b48dfe5bb301e693e139e3b6f22
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2022-07-13 14:37:06 -0400

    trabajando en el login

--- VHM.Web/src/app/components/login/login.component.ts
+++ VHM.Web/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    constructor(private auth:AuthService) { }
 
   ngOnInit(): void {
   }
+
+    logIn(model:User){
+	this.auth.login(model)
+    }
 
 }
