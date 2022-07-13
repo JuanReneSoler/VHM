1ad8170c59f04494ff693ff23be23572a5e6e816
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2022-07-13 14:02:31 -0400

    fixed: athentication

--- VHM.Web/src/app/app-routing.module.ts
+++ VHM.Web/src/app/app-routing.module.ts
@@ -1,21 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
-    {
-	path:"/",
-	redirectTo: "auth",
-	pathMatch: "full",
-	children:[
-	    {
-		path:"home",
-	    }
-	]
-    },
-    {
-	path:"auth",
-	loadChildren: ()=> import("./auth/auth.module").then(x => x.AuthModule)
-    }
 ];
 
 @NgModule({
